af9cb2cf7bd0742fa2c73f447a39a14cd52c8e90
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-05-15 18:42:42 +0200

    fixed refernce to jwtoken

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,8 +1,7 @@
 import * as jwt from 'jsonwebtoken';
-import * as config from 'config';
+import { config } from '../config';
 
-
-export default function(req, res, next) {
+export default function (req, res, next) {
     const header = req.header('Authorization');
     if (!header) return res.status(401).send('Access denied. No token provided!');
 
@@ -10,7 +9,7 @@ export default function(req, res, next) {
     const token = (parts.length > 1) ? parts[1] : '';
 
     try {
-        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        const decoded = jwt.verify(token, config.jwtPrivateKey);
         req.user = decoded;
         next();
     } catch (ex) {
