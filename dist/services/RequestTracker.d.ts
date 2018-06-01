70266966c9c06aa10d58604b41e148944337ac02
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-06-01 14:40:06 +0200

    rebuild to fix app.node.js prob

--- dist/services/RequestTracker.d.ts
+++ dist/services/RequestTracker.d.ts
@@ -1,7 +1,7 @@
 declare enum CommandState {
     RequestSent = 1,
     RequestConfirmed = 2,
-    SessionStarted = 3,
+    SessionStarted = 3
 }
 declare class RequestTracker {
     private requests;
