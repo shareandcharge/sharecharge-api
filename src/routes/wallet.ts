ec4faf0472a300ec2a37924353b314f8c6c20d34
stipa <stipa@motionwerk.com>
2018-07-10 14:57:00 +0200

    renamed pubKey to address and bumped up version

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -8,11 +8,11 @@ export default (wallet: Wallet)=> {
     router.get('/create', async (req, res) => {
         const wallet = Wallet.generate();
         console.log('Wallet created');
-        
-        const seed = wallet.seed;
-        const pubKey = wallet.wallet.keychain[0].address;
 
-        res.send({seed, pubKey});
+        const seed = wallet.seed;
+        const address = wallet.wallet.keychain[0].address;
+
+        res.send({seed, address});
     });
 
     return router;
