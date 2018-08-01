137f393d60f535d7463a8e4f473b713194d91a0d
adamstaveley <adam@motionwerk.com>
2018-08-01 12:16:52 +0200

    configurable server

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,6 @@
-#!/usr/bin/env node
+import server from './server';
 
-import { ShareCharge, Wallet } from '@motionwerk/sharecharge-lib';
-import { config } from './config';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import charging from './routes/charging';
-import cdr from './routes/cdr';
-import store from './routes/store';
-import token from './routes/token';
-import wallet_route from './routes/wallet';
+const port = parseInt(process.env.PORT || '3000');
+const host = process.env.HOST || '127.0.0.1';
 
-const app = express();
-
-const sc = ShareCharge.getInstance(config);
-sc.startListening();
-
-sc.on('Error', result => {
-    console.log("Error", result);
-});
-
-const wallet = new Wallet(config.seed);
-
-app.use(bodyParser.json()); // support json bodies
-app.use(bodyParser.urlencoded({extended: true}));  //support encoded bodies
-
-app.use('/health', (req, res) => res.send('OK'));
-
-app.use('/api/store', store(sc, wallet));
-app.use('/api/token', token(sc, wallet));
-app.use('/api/charging', charging(sc, wallet));
-app.use('/api/cdr', cdr(sc, wallet));
-app.use('/api/wallet', wallet_route(config, sc, wallet));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    // process.send({ msg: "started", args: ""});
-    console.log('API server running on http://localhost:' + port);
-    console.log(config.apiKey ? `API Key: ${config.apiKey}` : `Warning: No API Key found! Unauthorized access possible.`);
-});
+server(host, port);
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import { ShareCharge, Wallet } from '@motionwerk/sharecharge-lib';
+import { config } from './config';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+// import "reflect-metadata";
+import charging from './routes/charging';
+import cdr from './routes/cdr';
+import store from './routes/store';
+import token from './routes/token';
+import wallet_route from './routes/wallet';
+
+const app = express();
+
+const sc = ShareCharge.getInstance(config);
+
+const wallet = new Wallet(config.seed);
+
+app.use(bodyParser.json()); // support json bodies
+app.use(bodyParser.urlencoded({extended: true}));  //support encoded bodies
+
+app.use('/health', (req, res) => res.send('OK'));
+    
+app.use('/api/store', store(sc, wallet));
+app.use('/api/token', token(sc, wallet));
+app.use('/api/charging', charging(sc, wallet));
+app.use('/api/cdr', cdr(sc, wallet));
+app.use('/api/wallet', wallet_route(config, wallet));
+
+export default (host: string, port: number) => app.listen(port, host, () => {
+    // process.send({ msg: "started", args: ""});
+    console.log(`API server running on http://${host}:${port}`);
+    console.log(config.apiKey ? `API Key: ${config.apiKey}` : `Warning: No API Key found! Unauthorized access possible.`);
+    sc.startListening();
+    sc.on('Error', result => {
+        console.log("Error", result);
+    });
+});
