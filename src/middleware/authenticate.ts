91a04d3e360a92f35080ea0021a9d314ba4a691a
adamstaveley <adam@motionwerk.com>
2018-08-02 10:59:51 +0200

    fix binary, update README and preliminary swagger documentation

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,14 +1,14 @@
 import { config } from '../config';
 
 export default function (req, res, next) {
-    // if (config.apiKey) {
-    //     const header = req.header('Authorization');
-    //     if (!header) {
-    //         return res.status(401).send('Access denied. No token provided!');
-    //     }
-    //     const token = header.split(' ')[1];
-    //     token === config.apiKey ? next() : res.status(400).send('Invalid token.');
-    // } else {
+    if (config.apiKey) {
+        const header = req.header('Authorization');
+        if (!header) {
+            return res.status(401).send('Access denied. No token provided!');
+        }
+        const token = header.split(' ')[1];
+        token === config.apiKey ? next() : res.status(400).send('Invalid token.');
+    } else {
         next();
-    // }
+    }
 }
